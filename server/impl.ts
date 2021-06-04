1408f64f959ffa8a3b67ca260dbdcf4313cdbe96
Anuj Patel <anuj.patel@meraki.net>
2021-06-04 09:30:13 -0700

    basic server impl with ontick working

--- server/impl.ts
+++ server/impl.ts
@@ -1,31 +1,48 @@
 import { Methods, Context, Result } from "./.rtag/methods";
 import {
   UserData,
+  PlayerInfo,
   PlayerState,
   ICreateGameRequest,
   IJoinGameRequest,
   IAnswerQuestionRequest,
 } from "./.rtag/types";
 
-interface InternalState {}
+interface InternalState {
+  players: PlayerInfo[];
+}
 
 export class Impl implements Methods<InternalState> {
   createGame(user: UserData, ctx: Context, request: ICreateGameRequest): InternalState {
-    return {};
+    return {
+      players: []
+    };
   }
   joinGame(state: InternalState, user: UserData, ctx: Context, request: IJoinGameRequest): Result {
-    return Result.unmodified("Not implemented");
+    if (state.players.find((player) => player.name === user.name)) {
+      return Result.unmodified("Already joined");
+    }
+    state.players.push({name: user.name, score: 0});
+    return Result.modified();
   }
   answerQuestion(state: InternalState, user: UserData, ctx: Context, request: IAnswerQuestionRequest): Result {
-    return Result.unmodified("Not implemented");
+    var player = state.players.find((p) => p.name === user.name);
+    if (player === undefined) {
+      return Result.unmodified("Invalid player");
+    }
+
+    player.score += 1
+
+    return Result.modified();
   }
   getUserState(state: InternalState, user: UserData): PlayerState {
     return {
-      currentPlayer: "",
-      players: [],
+      currentPlayer: user.name,
+      players: state.players,
     };
   }
   onTick(state: InternalState, ctx: Context, timeDelta: number): Result {
+    console.log("Asdf");
     return Result.unmodified();
   }
 }
